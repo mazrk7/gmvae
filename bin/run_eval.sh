e91ecedd930df4eccafa0d4face7c33bcdda37a9
mazrk7 <mark_zolotas@hotmail.com>
2019-08-28 20:54:43 +0100

    Changed parameters for better clustering results

--- bin/run_eval.sh
+++ bin/run_eval.sh
@@ -3,7 +3,7 @@ WS_DIR=$HOME/workspace/gmvae
 python $WS_DIR/scripts/run_gmvae.py \
     --mode=eval \
     --model=gmvae \
-    --latent_size=64 \
+    --latent_size=128 \
     --hidden_size=512 \
     --num_layers=2 \
     --logdir="$WS_DIR/checkpoints" \
--- bin/run_train.sh
+++ bin/run_train.sh
@@ -3,13 +3,13 @@ WS_DIR=$HOME/workspace/gmvae
 python $WS_DIR/scripts/run_gmvae.py \
     --mode=train \
     --model=gmvae \
-    --latent_size=64 \
+    --latent_size=128 \
     --hidden_size=512 \
     --num_layers=2 \
     --logdir="$WS_DIR/checkpoints" \
     --summarise_every=1000 \
     --early_stop_rounds=10000 \
-    --batch_size=16 \
+    --batch_size=32 \
     --learning_rate=0.001 \
     --gpu_id="0" \
     --gpu_num="0"
